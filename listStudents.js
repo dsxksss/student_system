43aa5d6d65550532d9a850f86d0882b9c71b8183
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-11-27 12:18:03 +0800

    优化显示判断逻辑

--- listStudents.js
+++ listStudents.js
@@ -6,6 +6,14 @@ const listStudents = () => {
   // 判断是否已存在之前存入的数据
   if (existsSync(DATAPATH)) {
     const students = jsonfile.readFileSync(DATAPATH);
+
+    // 如果存在数据文件但是内容为空也不显示
+    if (students.length <= 0) {
+      console.log("\033[31m没有历史数据,请录入数据后再查看\033[0m");
+      // 结束操作
+      return;
+    }
+
     console.table(students);
     // table函数可以只查看部分数据
     // 下面这个例子就只查看name数据
